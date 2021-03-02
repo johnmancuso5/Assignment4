283c66464c2b1de43bd7e1ed96b18477f16cc898
John Mancuso <JohnMancuso5@gmail.com>
2021-03-02 00:03:27 -0500

    Adding the manual.md and Toast Conditions for Invalid Inputs

--- app/src/main/java/edu/qc/seclass/tipcalculator/TipCalculatorActivity.java
+++ app/src/main/java/edu/qc/seclass/tipcalculator/TipCalculatorActivity.java
@@ -2,9 +2,11 @@ package edu.qc.seclass.tipcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class TipCalculatorActivity extends AppCompatActivity {
 
@@ -15,32 +17,55 @@ public class TipCalculatorActivity extends AppCompatActivity {
     }
 
     public void clickedTip(View view){
+
+        // For the Toast
+        Context context = getApplicationContext();
+        CharSequence text = "Empty of incorrect value(s)";
+        int duration = Toast.LENGTH_LONG;
+
         String checkString = ((EditText)findViewById(R.id.checkAmountValue)).getText().toString();
         String partyString = ((EditText)findViewById(R.id.partySizeValue)).getText().toString();
 
-        int checkInt = Integer.parseInt(checkString);
+        if(checkString.equals("")){
+            Toast.makeText(this, text, duration).show();
+            return;
+        }
+
+        if(partyString.equals("")){
+            Toast.makeText(this, text, duration).show();
+            return;
+        }
+
+        double checkInt = Double.parseDouble(checkString);
         int partyInt = Integer.parseInt(partyString);
 
-        // 15%
-        ((EditText)findViewById(R.id.fifteenPercentTipValue)).setText(tipPerPerson(checkInt, partyInt, 15));
-        ((EditText)findViewById(R.id.fifteenPercentTotalValue)).setText(tipTotal(checkInt, partyInt, 15));
+        if(checkInt <= 0 && partyInt <= 0) {
+            Toast.makeText(this, text, duration).show();
+            return;
+        }
 
-        // 20%
-        ((EditText)findViewById(R.id.twentyPercentTipValue)).setText(tipPerPerson(checkInt, partyInt, 20));
-        ((EditText)findViewById(R.id.twentyPercentTotalValue)).setText(tipTotal(checkInt, partyInt, 20));
+        else {
+            // 15%
+            ((EditText) findViewById(R.id.fifteenPercentTipValue)).setText(tipPerPerson(checkInt, partyInt, 15));
+            ((EditText) findViewById(R.id.fifteenPercentTotalValue)).setText(tipTotal(checkInt, partyInt, 15));
 
-        // 25%
-        ((EditText)findViewById(R.id.twentyfivePercentTipValue)).setText(tipPerPerson(checkInt, partyInt, 25));
-        ((EditText)findViewById(R.id.twentyfivePercentTotalValue)).setText(tipTotal(checkInt, partyInt, 25));
+            // 20%
+            ((EditText) findViewById(R.id.twentyPercentTipValue)).setText(tipPerPerson(checkInt, partyInt, 20));
+            ((EditText) findViewById(R.id.twentyPercentTotalValue)).setText(tipTotal(checkInt, partyInt, 20));
+
+            // 25%
+            ((EditText) findViewById(R.id.twentyfivePercentTipValue)).setText(tipPerPerson(checkInt, partyInt, 25));
+            ((EditText) findViewById(R.id.twentyfivePercentTotalValue)).setText(tipTotal(checkInt, partyInt, 25));
+        }
     }
 
-    public String tipPerPerson(int total, int size, int percent){
+    public String tipPerPerson(double total, int size, int percent){
         double value = (total / size) * (percent / 100.0);
 
         return Integer.toString((int) Math.ceil(value));
     }
 
-    public String tipTotal(int total, int size, int percent){
+    public String tipTotal(double total, int size, int percent){
         double value = (total / size) + (total / size * (percent / 100.0));
 
         return Integer.toString((int) Math.ceil(value));
---
+++ manual.md
@@ -0,0 +1,11 @@
+Assignemnt 4:
+
+The TipCalculator takes in 2 inputs and calculates the tips at a certain percent. The tip is calculated per person and the total for the group.
+
+The inputs are for the check amount and party size. A Error Message will pop up on the android phone if the values of these inputs are less than 0 or null.
+
+Run the phone emulator or use an android phone to run the application. Once ran, you will be prompt with to input the necessary data and it will display below the Compute Tip button.
+
+The UI was setup for phones that are in 1080 x 1920 (Pixel 2).
+
+
